56c3c5654f6d5b299029ff9abf32f65238465dd1
Dominick Triola <dominick@ignovadesign.com>
2017-03-28 22:51:13 -0700

    initialize with datatypes

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // long width 64
+        long minLong = -9_223_372_036_854_775_808L;
+        long maxLong = 9_223_372_036_854_775_807L;
+
+        // int width = 32
+        int minInt = -2_147_483_648;
+        int maxInt = 2_147_483_647;
+
+        // short width = 16
+        short minShort = -32768;
+        short maxShort = 32767;
+
+        // byte width = 8
+        byte minByte = -128;
+        byte maxByte = 127;
+    }
+}
